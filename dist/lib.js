7693725083235702a2c854a2292af69b4a4bbe29
Lukas <lukasstrassel@googlemail.com>
2024-11-13 18:01:50 +0100

    fix: defer format for output

--- dist/lib.js
+++ dist/lib.js
@@ -31,25 +31,40 @@ function formatCoverage(coverage) {
   if (coverage > 0.8) color = "orange";
   if (coverage > 0.95) color = "lightgreen";
   if (coverage === 1) color = "green";
-  return "\\$\\${\\\\\\\\color{" + color + "}" + percent + "\\\\\\\\%}\\$\\$";
+  return "{\\color{" + color + "}" + percent + "\\%}";
 }
-function getCoverageLine({
-  hit,
-  found
-}) {
-  let percent = found == 0 ? 1 : hit / found;
-  return `${formatCoverage(percent)}<br />${hit} / ${found}`;
+function formatCoverageLine({ hit, found }, previousCoverage) {
+  const coverage = getCoverage({ hit, found });
+  const diff = !previousCoverage || coverage === previousCoverage ? 0 : (coverage - previousCoverage) / Math.abs(previousCoverage) * 100;
+  let formattedString = formatCoverage(coverage);
+  if (diff)
+    formattedString = "^" + new Intl.NumberFormat("en-US", {
+      maximumSignificantDigits: 2
+    }).format(diff) + "\\%" + formattedString;
+  return `$${formattedString}$<br />$${hit} / ${found}$`;
 }
 function findFile(report, file) {
   return report.find((r) => r.file === file);
+}
+function getCoverage({ hit, found }) {
+  return found == 0 ? 1 : hit / found;
 }
 function generateCoverageDiff(before, after) {
   let content = "| File | Line Coverage | Function Coverage | Branch Coverage |\n| --- | ---: | ---: | ---: |\n";
   for (const report of after) {
     const previousRunResult = findFile(before, report.file);
-    const lineCoverage = getCoverageLine(report.lines);
-    const functionCoverage = getCoverageLine(report.functions);
-    const branchCoverage = getCoverageLine(report.branches);
+    const lineCoverage = formatCoverageLine(
+      report.lines,
+      previousRunResult ? getCoverage(previousRunResult.lines) : 0
+    );
+    const functionCoverage = formatCoverageLine(
+      report.functions,
+      previousRunResult ? getCoverage(previousRunResult.functions) : 0
+    );
+    const branchCoverage = formatCoverageLine(
+      report.branches,
+      previousRunResult ? getCoverage(previousRunResult.branches) : 0
+    );
     content += `| ${report.file} | ${lineCoverage} | ${functionCoverage} | ${branchCoverage} |
 `;
   }
--- src/lib.ts
+++ src/lib.ts
@@ -8,22 +8,50 @@ function formatCoverage(coverage: number) {
   if (coverage > 0.8) color = "orange";
   if (coverage > 0.95) color = "lightgreen";
   if (coverage === 1) color = "green";
-  return "\\$\\${\\\\\\\\color{" + color + "}" + percent + "\\\\\\\\%}\\$\\$";
+  return "{\\color{" + color + "}" + percent + "\\%}";
 }
 
-function getCoverageLine({
-  hit,
-  found,
-}: {
+type GetCoverageLineParams = {
   hit: number;
   found: number;
-}): string {
-  let percent = found == 0 ? 1 : hit / found;
-  return `${formatCoverage(percent)}<br />${hit} / ${found}`;
+};
+
+const UP = `↑`;
+const DOWN = `↓`;
+
+function formatCoverageLine(
+  { hit, found }: GetCoverageLineParams,
+  previousCoverage: number,
+): string {
+  const coverage = getCoverage({ hit, found });
+  const diff =
+    !previousCoverage || coverage === previousCoverage
+      ? 0
+      : ((coverage - previousCoverage) / Math.abs(previousCoverage)) * 100;
+  let formattedString = formatCoverage(coverage);
+  if (diff)
+    formattedString =
+      "^" +
+      new Intl.NumberFormat("en-US", {
+        maximumSignificantDigits: 2,
+      }).format(diff) +
+      "\\%" +
+      formattedString;
+
+  return `$${formattedString}$<br />$${hit} / ${found}$`;
 }
 
 function findFile(report: Lcov, file: string) {
   return report.find((r) => r.file === file);
+}
+
+type GetCoverageParams = {
+  hit: number;
+  found: number;
+};
+
+function getCoverage({ hit, found }: GetCoverageParams) {
+  return found == 0 ? 1 : hit / found;
 }
 
 export function generateCoverageDiff(before: Lcov, after: Lcov) {
@@ -31,10 +59,18 @@ export function generateCoverageDiff(before: Lcov, after: Lcov) {
     "| File | Line Coverage | Function Coverage | Branch Coverage |\n| --- | ---: | ---: | ---: |\n";
   for (const report of after) {
     const previousRunResult = findFile(before, report.file);
-    // do sth
-    const lineCoverage = getCoverageLine(report.lines);
-    const functionCoverage = getCoverageLine(report.functions);
-    const branchCoverage = getCoverageLine(report.branches);
+    const lineCoverage = formatCoverageLine(
+      report.lines,
+      previousRunResult ? getCoverage(previousRunResult.lines) : 0,
+    );
+    const functionCoverage = formatCoverageLine(
+      report.functions,
+      previousRunResult ? getCoverage(previousRunResult.functions) : 0,
+    );
+    const branchCoverage = formatCoverageLine(
+      report.branches,
+      previousRunResult ? getCoverage(previousRunResult.branches) : 0,
+    );
 
     content += `| ${report.file} | ${lineCoverage} | ${functionCoverage} | ${branchCoverage} |\n`;
   }
